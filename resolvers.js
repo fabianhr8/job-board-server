e9dabb8e7ad30d76c048ed376d193c5d2b383664
fabian.hr8 <fabian.hr8@gmail.com>
2023-05-23 09:07:43 -0600

    add date and company to Jobs query

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,16 @@
 import { getJobs } from './db/jobs.js';
+import { getCompany } from './db/companies.js';
 
 export const resolvers = {
   Query: {
     jobs: async () => await getJobs()
   },
+  Job: {
+    date: async (job) => toISOString(job.createdAt),
+    company: (job) => getCompany(job.companyId)
+  },
 };
+
+const toISOString = (value) => {
+  return value.slice(0, 'yyyy-mm-dd'.length)
+}
